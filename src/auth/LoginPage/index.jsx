fccc5e638b1b2e0ada5c5592974c2c09ca5a1b56
Adeshina Qudus <qudusa55@gmail.com>
2024-03-20 16:51:36 +0100

    first commit

--- src/auth/LoginPage/index.jsx
+++ src/auth/LoginPage/index.jsx
@@ -1,27 +1,35 @@
 import style from "./index.module.css"
 import webpix from "../../asset/auth/login/webpix.png";
 import FilledButton from "../../reuseable/filledButton";
+import {Link} from "react-router-dom";
 
 
 const LoginPage = ()=> {
     return(
     <div className={style.wholePage}>
         <div className={style.image}>
-            <img src={webpix} alt={"rectangle"} height={600} width={484}/>
+            <img src={webpix} alt={"rectangle"} height={632} width={700}/>
         </div>
         <div className={style.inputPage}>
             <h3>Welcome Back!</h3>
             <p>Log in to your Dashboard</p>
             <form>
-                <input type={"text"} placeholder={"Email :"}/>
-                <input type={"text"} placeholder={"Password :"}/>
+                <input type={"text"} placeholder={"Email :"}  className={style.inputBox}/>
+                <input type={"text"} placeholder={"Password :"} className={style.inputBox}/>
             </form>
-            {/*<div className={style.already}>*/}
-            {/*    <p color={"rgba(196, 196, 196, 1)"}>Already have an Account?</p>*/}
+            <div className={style.secondForm}>
+                <form className={style.remember}>
+                    <input type={"checkbox"}/>
+                    <p className={style.me}>Remember Me</p>
+                </form>
 
-            {/*</div>*/}
+            </div>
             <FilledButton
                 color={"white"} text={"Login"} buttonColor={"rgba(85, 34, 158, 1)"}/>
+            <div className={style.last}>
+                <p color={"rgba(196, 196, 196, 1)"}>Don’t have an Account?</p>
+                <Link to={"/sign-up"} className={style.linking}>Sign Up</Link>
+            </div>
         </div>
     </div>
     )
--- src/auth/signuppage/index.jsx
+++ src/auth/signuppage/index.jsx
@@ -1,28 +1,31 @@
 import style from "./index.module.css"
 import webpix from "../../asset/auth/signup/webpix.png"
 import FilledButton from "../../reuseable/filledButton";
+import {Link} from "react-router-dom";
 const WebSignUpPage = ()=>{
 
     return(
         <div className={style.wholePage}>
             <div className={style.image}>
-                <img src={webpix} alt={"rectangle"} height={600} width={484}/>
+                <img src={webpix} alt={"rectangle"} height={630} width={700}/>
             </div>
             <div className={style.inputPage}>
                 <h3>Welcome!</h3>
                 <p>Sign up by entering the information below</p>
                 <form>
-                    <input type={"text"} placeholder={"First Name :"}/>
-                    <input type={"text"} placeholder={"Last Name :"}/>
-                    <input type={"text"} placeholder={"Email :"}/>
-                    <input type={"text"} placeholder={"Password :"}/>
+                    <input type={"text"} placeholder={"First Name :"} className={style.value}/>
+                    <input type={"text"} placeholder={"Last Name :"} className={style.value}/>
+                    <input type={"text"} placeholder={"Email :"} className={style.value}/>
+                    <input type={"text"} placeholder={"Password :"} className={style.value}/>
                 </form>
                 <div className={style.already}>
                     <p color={"rgba(196, 196, 196, 1)"}  >Already have an Account?</p>
-
+                    <Link to={"/login"} className={style.linking}>Login</Link>
                 </div>
+                <div className={style.fillBut}>
                 <FilledButton
                     color={"white"} text={"Sign Up"} buttonColor={"rgba(85, 34, 158, 1)"}/>
+                </div>
             </div>
         </div>
     )
